31cbeb9a796fcd0f188c8b9fcb49bf74d481cbf1
xzchen <xzchen65@126.com>
2017-04-26 00:24:21 +0800

    resume加载图片“速度”优化
    这样就会导致一开始的加载时间变长。一段事件内hover imgdemo会是css部分。后面才能出现js的事件。

--- resume/main.js
+++ resume/main.js
@@ -2,40 +2,29 @@ window.onload = function () {
 	var imgs = document.querySelectorAll("main img");
 	var imgsHref = []  //记录每张展示demo图片所关联的a的在线demo链接
 	var imgsSrc = [];
-	var showImages = document.getElementById('showImages');
-	var showImage = document.getElementById("showImage");
+	var showImageContainer = document.getElementById('showImageContainer');
+	var showImages = showImageContainer.getElementsByTagName('img');
 	for (var i = 0; i < imgs.length; i++) {
 		imgs[i].index = i;  //根据此上传的index从imgsHref下获得链接地址
 		imgsHref.push(imgs[i].parentNode.getElementsByTagName('a')[0].href)
 		imgsSrc.push(imgs[i].src.replace("png","gif"))
 		imgs[i].onmouseover = function (i) {
-			// showImage.src = this.index!==0?this.src:imgs[1].src;  //onmouseover状态出现大图展示demo动态效果。其余的还没录，先就这样。
-			// 这里视频没录制完成，先这么写着。
-			if (this.index === 0 || this.index === 2) {
-				showImage.src = imgsSrc[this.index]
+			showImageContainer.style.zIndex = 0;
+			for (var i = 0; i < showImages.length; i++) {
+				showImages[i].style.zIndex = 0;
 			}
-			else {
-				showImage.src = this.src
-			}  
-			showImage.index = this.index;
-			showImages.style.zIndex = 0;
+			showImages[this.index].style.zIndex = 1;
 		}
 	}
 
-	showImage.onclick = function (ev) {
-		ev = ev || window.event;
-		this.parentNode.href = imgsHref[this.index];
-		if (ev.stopPropagation) {
-			ev.stopPropagation();
-		}
-		else {
-			ev.cancelBubble = true;
+	for (var i = 0; i < showImages.length; i++) {
+		showImages[i].index = i;
+		showImages[i].onclick = function () {
+			this.parentNode.href = imgsHref[this.index];
 		}
 	}
-
-	showImages.onclick = function () {
+	showImageContainer.onclick = function () {
 		this.style.zIndex =  -1;
-		showImage.src = "";
 	}
 }
 
